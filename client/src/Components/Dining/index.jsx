e2bcf1512f822358d8c0cdcd5e8b74b8e60769ef
Pragnan143 <pragnanmanam8@gmail.com>
2023-12-29 01:01:41 +0530

    DiningCarousel work Done

--- client/src/Components/Dining/index.jsx
+++ client/src/Components/Dining/index.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+
+// components
 import DiningCarousel from "./DiningCarousel";
 
-const DinOut = () => {
+const Dining = () => {
   return (
-    <>
+    <div className="mb-10">
+      <h1 className="text-xl my-4 md:my-8 md:text-3xl md:font-semibold">
+        Collections
+      </h1>
       <DiningCarousel />
-    </>
+    </div>
   );
 };
 
-export default DinOut;
+export default Dining;
